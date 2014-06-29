eb2680b5e6d4b48ed9b952b0ed1d5d470050ec62
SreeMyntra <sreenath.s@myntra.com>
2014-06-29 11:34:26 +0530

    Adding myntra

--- index.php
+++ index.php
@@ -23,34 +23,34 @@
 
 		<div class="paralax-container grey-gradiant center">
 			<div class=inline-block>
-				<img src="/images/techIcons/ruby.png" x="1400" style="left:835px;top:500px"/>
-				<img src="/images/techIcons/unity.png" x="1200" style="left:630px;top:480px"/>
-				<img src="/images/techIcons/shell.png" x="-50" style="left:135px;top:250px"/>
-				<img src="/images/techIcons/php.png" x="-10" style="left:160px;top:320px"/>
-				<img src="/images/techIcons/java.png" x="1000" style="left:805px;top:240px"/>
-				<img src="/images/techIcons/c.png" x="900" style="left:750px;top:70px"/>
-				<img src="/images/techIcons/cpp.png" x="900" style="left:695px;top:110px"/>
+				<div class="img-float" x="1400" style="left:835px;top:500px;background-image: url('/images/techIcons/ruby.png');width:70px;height:88px;"/>&nbsp;</div>
+				<div class="img-float" x="1200" style="left:630px;top:480px;background-image: url('/images/techIcons/unity.png');width:220px;height:101px;"/>&nbsp;</div>
+				<div class="img-float" x="-50" style="left:135px;top:250px;background-image: url('/images/techIcons/shell.png');width:100px;height:84px;"/>&nbsp;</div>
+				<div class="img-float" x="-10" style="left:160px;top:320px;background-image: url('/images/techIcons/php.png');width:120px;height:70px;"/>&nbsp;</div>
+				<div class="img-float" x="1000" style="left:805px;top:240px;background-image: url('/images/techIcons/java.png');width:170px;height:95px;"/>&nbsp;</div>
+				<div class="img-float" x="900" style="left:750px;top:70px;background-image: url('/images/techIcons/c.png');width:60px;height:71px;"/>&nbsp;</div>
+				<div class="img-float" x="900" style="left:695px;top:110px;background-image: url('/images/techIcons/cpp.png');width:170px;height:84px;"/>&nbsp;</div>
 
-				<img src="/images/techIcons/jquery.png" x="900" style="left:700px;top:350px"/>
-				<img src="/images/techIcons/css3.png" x="800" style="left:620px;top:330px"/>
-				<img src="/images/techIcons/html5.png" x="700" style="left:510px;top:330px;"/> 
-				<img src="/images/techIcons/javascript.png" x="900" style="left:620px;top:430px"/>
+				<div class="img-float" x="900" style="left:700px;top:350px;background-image: url('/images/techIcons/jquery.png');width:174px;height:59px;"/>&nbsp;</div>
+				<div class="img-float" x="800" style="left:620px;top:330px;background-image: url('/images/techIcons/css3.png');width:90px;height:120px;"/>&nbsp;</div>
+				<div class="img-float" x="700" style="left:510px;top:330px;background-image: url('/images/techIcons/html5.png');width:127px;height:170px;"/>&nbsp;</div> 
+				<div class="img-float" x="900" style="left:620px;top:430px;background-image: url('/images/techIcons/javascript.png');width:220px;height:53px;"/>&nbsp;</div>
 
-				<img src="/images/techIcons/starling.png" x="900" style="left:590px;top:180px;"/> 
-				<img src="/images/techIcons/as3.png" x="900" style="left:495px;top:100px;"/>
-				<img src="/images/me.png" x="0" style="left:60px;top:0px;pointer-events:none;"/>
+				<div class="img-float" x="900" style="left:590px;top:180px;background-image: url('/images/techIcons/starling.png');width:216px;height:124px;"/>&nbsp;</div>
+				<div class="img-float" x="900" style="left:495px;top:100px;background-image: url('/images/techIcons/as3.png');width:160px;height:160px;"/>&nbsp;</div>
+				<div class="img-float" x="0" style="left:60px;top:0px;pointer-events:none;"><img src="/images/me.png"/></div>
 			</div>
 		</div>
 
 		<script> initiateImgParallax($(".paralax-container")); </script>
 
-		<div class="center bg1 v-padding" style="height:390px;">
+		<div class="center bg1 v-padding" style="height:250px;">
 			<div class="inline-block v-padding">
 				<div class=center>
 					<div class=sub-heading style="border-bottom:1px solid #CCCCCC;">My Current Assignment</div>
 					<div class="v-padding description">
-						<a href="http://apps.facebook.com/playhiddenshadows"><img src="/images/hs.png"></br></a>
-						I'm working on Zynga's latest game in the hidden objects genre, <i>Hidden Shadows</i>. Our team is behind both the web and mobile versions of the game. My tasks include creation of some key features, and polishing the game for better user experience. I have also taken up the task to crack starling framework for creating new and better UI components.
+						<a href="http://www.myntra.com/"><img src="/images/myntra.png"></br></a>
+						Myntra is an Indian online shopping retailer of fashion and casual lifestyle products. We currently offers close to 50,000 products from more than 600 Indian and international brands, and ships about 5 million goods per month by hosting around 3,000 sellers.
 					</div>
 				</div>
 			</div>
